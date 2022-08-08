154d6df27e2e1f06045b50625e84fc25258f8bb5
Rizky Khoiruddin <rizky.khoiruddin17@gmail.com>
2022-08-08 15:48:13 +0700

    8/8/2022

--- resources/views/user/admin/dashboard.blade.php
+++ resources/views/user/admin/dashboard.blade.php
@@ -78,18 +78,18 @@
 
       {{-- Content --}}
       <div class="col">
-        <div class="row head py-4 align-self-center">
-          <div class="col-6 ps-md-5 ps-3">
+        <div class="row head py-4 align-items-center">
+          <div class="col-md-6 col-10 ps-md-5 ps-3">
             <h4 class="">Admin Dashboard</h4>
           </div>
-          <div class="col-6 pe-md-5 pe-3">
-            <div class="head-content d-flex flex-row align-items-center justify-content-end gap-4">
-              <a class="help d-flex flex-row align-items-center gap-2 pt-1" href="">
+          <div class="col-md-6 col-2 pe-md-5 pe-3">
+            <div class="head-content d-flex flex-row align-items-center justify-content-end gap-md-4 gap-2">
+              <a class="help d-flex flex-row align-items-center gap-md-2 gap-1" href="">
                 <img class="img-fluid" src="/assets/help-grey.png" alt="">
-                <h6>Help</h6>
+                <h6 class="d-none d-md-inline">Help</h6>
               </a>
               <a href="">
-                <h6 class="pt-1">Admin Name</h6>
+                <h6 class="pt-1 d-none d-md-inline">Admin Name</h6>
               </a>
             </div>
           </div>
