565347ce1a7acfa378064da40880e3422d10c968
unknown <manuel.eric.b@gmail.com>
2022-11-14 10:22:26 +0700

    essay ongoing admin

--- resources/views/user/admin/essay-list/essay-ongoing.blade.php
+++ resources/views/user/admin/essay-list/essay-ongoing.blade.php
@@ -54,7 +54,7 @@
                     <th scope="row">{{ $i++ }}</th>
 
                     <td>{{ isset($essay->client_by_id) ? $essay->client_by_id->first_name.' '.$essay->client_by_id->last_name : $essay->client_by_email->first_name.' '.$essay->client_by_email->last_name }}</td>
-                    <td>{{ isset($essay->client_by_id) ? $essay->client_by_id->mentors->first_name.' '.$essay->client_by_id->mentors->last_name : $essay->client_by_email->mentors->first_name.' '.$essay->client_by_email->mentors->last_name }}</td>
+                    <td>{{ isset($essay->mentors) ? $essay->mentors->first_name.' '.$essay->mentors->last_name : '-' }}</td>
                     
                     {{-- <td>{{ $essay->status_essay_clients == 0 ? '-' : $essay->editor->first_name.' '.$essay->editor->last_name }}</td> --}}
                     <td>
