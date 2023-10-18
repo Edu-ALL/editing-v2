560b08a22b8b98d7477a4e3a255ba8b7cb76a52e
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-10-18 14:48:53 +0700

    optimizing code

--- app/Http/Traits/SendBotTelegramTrait.php
+++ app/Http/Traits/SendBotTelegramTrait.php
@@ -22,7 +22,7 @@ trait SendBotTelegramTrait {
         $client->request('GET', $url, [
             'json' =>[
             "chat_id" => $teleId, 
-            "text" => 'Essay Editing_' . date('y/m/d') .' '. substr($message, 0, 4000) . ' ' . $link, # Limit message telegram
+            "text" => 'Essay_Editing_' . date('y/m/d') .' '. substr($message, 0, 4000) . ' ' . $link, # Limit message telegram
             ]
         ]);
     }
