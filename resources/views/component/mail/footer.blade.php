e2e0654f722a865ec33045acafcf7ebd8d8dcbdd
ahmadfauzan <fauzana566@gmail.com>
2024-08-06 14:35:23 +0700

    Change brand logo & email template

---
+++ resources/views/component/mail/footer.blade.php
@@ -0,0 +1,111 @@
+<table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#ff5e56">
+    <tr>
+        <td class="content-spacing" style="font-size:0pt; line-height:0pt; text-align:left" width="30"></td>
+        <td>
+            <table width="100%" border="0" cellspacing="0" cellpadding="0" class="spacer"
+                style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                <tr>
+                    <td height="30" class="spacer"
+                        style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                        &nbsp;</td>
+                </tr>
+            </table>
+
+            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                    <!-- Column -->
+                    <th class="column-top"
+                        style="font-size:0pt; line-height:0pt; padding:0; margin:0; font-weight:normal; vertical-align:top; Margin:0">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td>
+                                    <div class="h2-white-m-center"
+                                        style="color:#ffffff; font-family:Arial,sans-serif; font-size:22px; line-height:26px; text-align:left; font-weight:bold; text-transform:uppercase">
+                                        {{ env('COMPANY_NAME') }}</div>
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0" class="spacer"
+                                        style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                                        <tr>
+                                            <td height="4" class="spacer"
+                                                style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                                                &nbsp;</td>
+                                        </tr>
+                                    </table>
+
+                                    <div class="text-white-m-center"
+                                        style="color:#ffffff; font-family:Arial,sans-serif; font-size:12px; line-height:20px; text-align:left">
+                                        Jl Jeruk Kembar Blok Q9 No. 15 Srengseng, Kembangan
+                                        <br> Jakarta Barat, 11630
+                                    </div>
+                                    <div style="font-size:0pt; line-height:0pt;" class="mobile-br-15"></div>
+
+                                </td>
+                            </tr>
+                        </table>
+                    </th>
+                    <!-- END Column -->
+                </tr>
+            </table>
+            <table width="100%" border="0" cellspacing="0" cellpadding="0" class="spacer"
+                style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                <tr>
+                    <td height="26" class="spacer"
+                        style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                        &nbsp;</td>
+                </tr>
+            </table>
+
+            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                    <!-- Column -->
+                    <th class="column-top"
+                        style="font-size:0pt; line-height:0pt; padding:0; margin:0; font-weight:normal; vertical-align:top; Margin:0"
+                        width="320">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td>
+                                    <div class="text-white-m-center"
+                                        style="color:#ffffff; font-family:Arial,sans-serif; font-size:12px; line-height:20px; text-align:left">
+                                        <a href="#" target="_blank" class="link-white"
+                                            style="color:#ffffff; text-decoration:none"><span class="link-white"
+                                                style="color:#ffffff; text-decoration:none">Problems
+                                                or questions ?</span></a>
+                                        <br />Email: <a href="#" target="_blank" class="link-white"
+                                            style="color:#ffffff; text-decoration:none"><span class="link-white"
+                                                style="color:#ffffff; text-decoration:none">{{ env('COMPANY_MAIL') }}</span></a>
+                                        {{-- <br />Phone: <a href="tel:+1123456789"
+                                                                        target="_blank" class="link-white"
+                                                                        style="color:#ffffff; text-decoration:none"><span
+                                                                            class="link-white"
+                                                                            style="color:#ffffff; text-decoration:none">+1
+                                                                            (123) 456-789</span></a> --}}
+                                    </div>
+                                    <div style="font-size:0pt; line-height:0pt;" class="mobile-br-15"></div>
+
+                                </td>
+                            </tr>
+                        </table>
+                    </th>
+
+                </tr>
+            </table>
+            <table width="100%" border="0" cellspacing="0" cellpadding="0" class="spacer"
+                style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                <tr>
+                    <td height="30" class="spacer"
+                        style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                        &nbsp;</td>
+                </tr>
+            </table>
+
+        </td>
+        <td class="content-spacing" style="font-size:0pt; line-height:0pt; text-align:left" width="30"></td>
+    </tr>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" class="spacer"
+    style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+    <tr>
+        <td height="20" class="spacer"
+            style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+            &nbsp;</td>
+    </tr>
+</table>
---
+++ resources/views/component/mail/header.blade.php
@@ -0,0 +1,48 @@
+<table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#ffffff">
+    <tr>
+        <td class="content-spacing" style="font-size:0pt; line-height:0pt; text-align:left"
+            width="20"></td>
+        <td>
+            <table width="100%" border="0" cellspacing="0" cellpadding="0" class="spacer"
+                style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                <tr>
+                    <td height="32" class="spacer"
+                        style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                        &nbsp;</td>
+                </tr>
+            </table>
+
+            <div class="fluid-img-logo"
+                style="font-size:0pt; line-height:0pt; text-align:left">
+                <div class="img-center"
+                    style="font-size:0pt; line-height:0pt; text-align:center"><a href="#"
+                        target="_blank"><img
+                            src="{{ asset('assets/logo.webp') }}"
+                            border="0" width="200" height="auto" /></a>
+                </div>
+            </div>
+
+            <table width="100%" border="0" cellspacing="0" cellpadding="0" class="spacer"
+                style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                <tr>
+                    <td height="32" class="spacer"
+                        style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+                        &nbsp;
+
+                    </td>
+                </tr>
+            </table>
+
+        </td>
+        <td class="content-spacing" style="font-size:0pt; line-height:0pt; text-align:left"
+            width="20"></td>
+    </tr>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" class="spacer"
+    style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+    <tr>
+        <td height="20" class="spacer"
+            style="font-size:0pt; line-height:0pt; text-align:center; width:100%; min-width:100%">
+            &nbsp;</td>
+    </tr>
+</table>
