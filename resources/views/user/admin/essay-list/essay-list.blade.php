8d63f523c7942667b02f10ab58c417541a0d40db
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2025-01-23 13:25:08 +0700

    update essay list

--- resources/views/user/admin/essay-list/essay-list.blade.php
+++ resources/views/user/admin/essay-list/essay-list.blade.php
@@ -29,10 +29,7 @@
                     <div class="row">
                         <div class="col-md col-12 p-0 studentList">
                             <div class="headline d-flex justify-content-between"
-                            @if (request()->is('admin/essay-list/completed'))
-                                style="background-color: var(--green)"
-                            @endif
-                            >
+                                @if (request()->is('admin/essay-list/completed')) style="background-color: var(--green)" @endif>
                                 <div class="col-md-6 col-7 d-flex align-items-center gap-md-3 gap-2">
                                     @if (request()->is('admin/essay-list/ongoing'))
                                         <img src="/assets/ongoing-essay.png" alt="">
@@ -91,68 +88,77 @@
 @endsection
 
 @section('js')
-<script>
-    $(document).ready(function() {
-        var pathname = window.location.pathname;
-        var route;
-        if (pathname == '/admin/essay-list/ongoing') {
-            route = '{{ route('data-ongoing-essay') }}'
-        } else if (pathname == '/admin/essay-list/completed'){
-            route = '{{ route('data-completed-essay') }}'
-        }
-        $('#listessay').DataTable({
-            scrollX: true,
-            responsive: true,
-            processing: true,
-            serverSide: true,
-            ajax: route,
-            columns: [{
-                    data: 'DT_RowIndex',
-                    name: 'DT_RowIndex',
-                    class: 'text-center'
-                },
-                {
-                    data: 'student_name',
-                    name: 'student_name'
-                },
-                {
-                    data: 'mentor_name',
-                    name: 'mentor_name'
-                },
-                {
-                    data: 'editor_name',
-                    name: 'editor_name'
-                },
-                {
-                    data: 'essay_title',
-                    name: 'essay_title'
-                },
-                {
-                    data: 'essay_deadline',
-                    name: 'essay_deadline'
-                },
-                {
-                    data: 'status',
-                    name: 'status'
-                },
-            ],
-            rowCallback: function(row, data) {
-                $(row).on('click', function() {
-                    if (pathname == '/admin/essay-list/ongoing') {
-                        window.open(`/admin/essay-list/ongoing/detail/${data.id_essay_clients }`, '_blank');
-                        // window.location.href = `/admin/essay-list/ongoing/detail/${data.id_essay_clients }`;
-                    } else if (pathname == '/admin/essay-list/completed'){
-                        window.open(`/admin/essay-list/completed/detail/${data.id_essay_clients }`, '_blank');
-                        // window.location.href = `/admin/essay-list/completed/detail/${data.id_essay_clients }`;
-                    }
-                });
+    <script>
+        $(document).ready(function() {
+            var pathname = window.location.pathname;
+            var route;
+            if (pathname == '/admin/essay-list/ongoing') {
+                route = '{{ route('data-ongoing-essay') }}'
+            } else if (pathname == '/admin/essay-list/completed') {
+                route = '{{ route('data-completed-essay') }}'
             }
+            $('#listessay').DataTable({
+                scrollX: true,
+                responsive: true,
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: route,
+                    error: function(xhr, error, thrown) {
+                        console.log('AJAX error:', error, thrown);
+                    }
+                },
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        class: 'text-center'
+                    },
+                    {
+                        data: 'student_name',
+                        name: 'student_name'
+                    },
+                    {
+                        data: 'mentor_name',
+                        name: 'mentor_name'
+                    },
+                    {
+                        data: 'editor_name',
+                        name: 'editor_name'
+                    },
+                    {
+                        data: 'essay_title',
+                        name: 'essay_title'
+                    },
+                    {
+                        data: 'essay_deadline',
+                        name: 'essay_deadline'
+                    },
+                    {
+                        data: 'status',
+                        name: 'status'
+                    },
+                ],
+                rowCallback: function(row, data) {
+                    $(row).on('click', function() {
+                        if (pathname == '/admin/essay-list/ongoing') {
+                            window.open(
+                                `/admin/essay-list/ongoing/detail/${data.id_essay_clients }`,
+                                '_blank');
+                            // window.location.href = `/admin/essay-list/ongoing/detail/${data.id_essay_clients }`;
+                        } else if (pathname == '/admin/essay-list/completed') {
+                            window.open(
+                                `/admin/essay-list/completed/detail/${data.id_essay_clients }`,
+                                '_blank');
+                            // window.location.href = `/admin/essay-list/completed/detail/${data.id_essay_clients }`;
+                        }
+                    });
+                }
+            });
         });
-    });
-</script>
-<script>
-    $(document).ready(function() {
-        $("#info-essay").modal('show');
-    });
-</script>
+    </script>
+    <script>
+        $(document).ready(function() {
+            $("#info-essay").modal('show');
+        });
+    </script>
 @stop
